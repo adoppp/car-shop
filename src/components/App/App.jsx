580a5d95c4a7baadc188b394ed9d017d3c28bf80
Adop <chdn786od@gmail.com>
2023-12-03 00:54:32 +0100

    routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,39 @@
-export const App = () => {
-  return <div>1</div>;
-};
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+
+import Main from "pages/Main/Main";
+import ROUTER from "utils/router";
+
+const router = createBrowserRouter([
+  {
+    path: ROUTER.MAIN,
+    element: <Main />,
+    errorElement: <Navigate to={ROUTER.MAIN} />,
+    children: [
+      {
+        index: true,
+        async lazy() {
+          let { Home } = await import("pages/Home/Home");
+          return { Component: Home };
+        },
+      },
+      {
+        path: ROUTER.CATALOG,
+        async lazy() {
+          let { Catalog } = await import("pages/Catalog/Catalog");
+          return { Component: Catalog };
+        },
+      },
+      {
+        path: ROUTER.FAVORITE,
+        async lazy() {
+          let { Favorite } = await import("pages/Favorite/Favorite");
+          return { Component: Favorite };
+        },
+      },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
---
+++ src/utils/router.js
@@ -0,0 +1,7 @@
+const ROUTER = {
+  MAIN: "/",
+  CATALOG: "catalog",
+  FAVORITE: "favorite",
+};
+
+export default ROUTER;
